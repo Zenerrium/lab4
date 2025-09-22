5e5a552fe9d1971dd274cfa88c173f67f01912eb
Томбасов Данила Кириллович <124450021+Zenerrium@users.noreply.github.com>
2025-09-22 12:31:11 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите количество чисел");
+int n = Convert.ToInt32(Console.ReadLine());
+double a;
+int s = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введите число:");
+    a = Convert.ToDouble(Console.ReadLine());
+    if (a%2 !=0)
+    {
+        s++;
+    }
+}
+Console.WriteLine(s);
